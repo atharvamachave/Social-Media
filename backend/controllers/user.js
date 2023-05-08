59e974b3cb701ad13bf20f90db444b2eefa75ef8
atharvamachave <machaveatharva@gmail.com>
2023-05-08 10:01:36 +0530

    Profile picture final commit

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,9 +4,11 @@ const {
   validateUsername,
 } = require('../helpers/validation');
 const User = require('../models/User');
+const Post = require('../models/Post');
 const Code = require('../models/Code');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const cloudinary = require('cloudinary');
 const { generateToken } = require('../helpers/tokens');
 const { sendVerificationEmail, sendResetCode } = require('../helpers/mailer');
 const generateCode = require('../helpers/generateCode');
@@ -222,6 +224,7 @@ exports.validateResetCode = async (req, res) => {
 
 exports.changePassword = async (req, res) => {
   const { email, password } = req.body;
+
   const cryptedPassword = await bcrypt.hash(password, 12);
   await User.findOneAndUpdate(
     { email },
@@ -230,4 +233,33 @@ exports.changePassword = async (req, res) => {
     }
   );
   return res.status(200).json({ message: 'ok' });
+};
+
+exports.getProfile = async (req, res) => {
+  try {
+    const { username } = req.params;
+    const profile = await User.findOne({ username }).select('-password');
+    if (!profile) {
+      return res.json({ ok: false });
+    }
+    const posts = await Post.find({ user: profile._id })
+      .populate('user')
+      .sort({ createdAt: -1 });
+    res.json({ ...profile.toObject(), posts });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateProfilePicture = async (req, res) => {
+  try {
+    const { url } = req.body;
+
+    await User.findByIdAndUpdate(req.user.id, {
+      picture: url,
+    });
+    res.json(url);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
